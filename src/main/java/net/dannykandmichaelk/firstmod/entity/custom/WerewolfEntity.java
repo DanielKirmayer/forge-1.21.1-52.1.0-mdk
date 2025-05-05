8262dee65dc9ab7fc6b14c06ec6f761895c76320
BT_2S14_11 <michaelk368@nycstudents.net>
2025-05-05 10:46:40 -0400

    Fixed sitting animation not working

--- src/main/java/net/dannykandmichaelk/firstmod/entity/custom/WerewolfEntity.java
+++ src/main/java/net/dannykandmichaelk/firstmod/entity/custom/WerewolfEntity.java
@@ -1,31 +1,44 @@
 package net.dannykandmichaelk.firstmod.entity.custom;
 
+import com.google.common.annotations.VisibleForTesting;
 import net.dannykandmichaelk.firstmod.entity.ModEntities;
 import net.dannykandmichaelk.firstmod.item.ModItems;
 import net.minecraft.core.component.DataComponents;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.AgeableMob;
-import net.minecraft.world.entity.AnimationState;
-import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
+import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.animal.camel.Camel;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.monster.ZombifiedPiglin;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.gameevent.GameEvent;
 import org.jetbrains.annotations.Nullable;
 
-public class WerewolfEntity extends Animal {
+
+
+public class WerewolfEntity extends Camel {
     public final AnimationState idleAnimationState = new AnimationState();
     private int idleAnimationTimeout = 0;
     public final AnimationState sitAnimationState = new AnimationState();
+    private static final EntityDimensions SITTING_DIMENSIONS = EntityDimensions.scalable(EntityType.CAMEL.getWidth(), EntityType.CAMEL.getHeight() - 1.43F)
+            .withEyeHeight(0.845F);
 
-    public WerewolfEntity(EntityType<? extends Animal> pEntityType, Level pLevel) {
+    public WerewolfEntity(EntityType<? extends Camel> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
     }
 
@@ -34,60 +47,49 @@ public class WerewolfEntity extends Animal {
         this.goalSelector.addGoal(0, new FloatGoal(this));
 
         this.goalSelector.addGoal(1, new PanicGoal(this, 2.0));
-        this.goalSelector.addGoal(2, new BreedGoal(this, 1.0));
         this.goalSelector.addGoal(3, new TemptGoal(this, 1.25, stack -> stack.is(ModItems.CRYONITE.get()), false));
 
-        this.goalSelector.addGoal(4, new FollowParentGoal(this, 1.25));
 
         this.goalSelector.addGoal(5, new WaterAvoidingRandomStrollGoal(this, 1.0));
         this.goalSelector.addGoal(6, new LookAtPlayerGoal(this, Player.class, 6.0F));
         this.goalSelector.addGoal(7, new RandomLookAroundGoal(this));
+        this.goalSelector.addGoal(1, new MeleeAttackGoal(this, 1.0, false));
+        this.targetSelector.addGoal(1, new HurtByTargetGoal(this).setAlertOthers(ZombifiedPiglin.class));
+        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, true));
     }
 
     public static AttributeSupplier.Builder createAttributes() {
         return Animal.createLivingAttributes()
                 .add(Attributes.MAX_HEALTH, 30D)
                 .add(Attributes.MOVEMENT_SPEED, 0.35D)
-                .add(Attributes.FOLLOW_RANGE, 24D);
+                .add(Attributes.FOLLOW_RANGE, 24D)
+                .add(Attributes.ATTACK_DAMAGE, 300);
     }
 
 
 
     @Override
-    public InteractionResult mobInteract(Player pPlayer, InteractionHand pHand) {
-        ItemStack itemstack = pPlayer.getItemInHand(pHand);
-        Item item = itemstack.getItem();
-        if (!this.level().isClientSide || this.isBaby() && this.isFood(itemstack)) {
-            if (this.isFood(itemstack) && this.getHealth() < this.getMaxHealth()) {
-                itemstack.consume(1, pPlayer);
-                FoodProperties foodproperties = itemstack.get(DataComponents.FOOD);
-                float f = foodproperties != null ? (float) foodproperties.nutrition() : 1.0F;
-                this.heal(2.0F * f);
-                return InteractionResult.sidedSuccess(this.level().isClientSide());
-            }
-        }
-        this.sitAnimationState.start(100);
-        return InteractionResult.FAIL;
+    public EntityDimensions getDefaultDimensions(Pose pPose) {
+        return pPose == Pose.SITTING ? SITTING_DIMENSIONS.scale(this.getAgeScale()) : super.getDefaultDimensions(pPose);
     }
 
-    @Override
-    public boolean isFood(ItemStack pStack) {
-        return pStack.is(ModItems.CRYONITE.get());
-    }
 
-    @Nullable
-    @Override
-    public AgeableMob getBreedOffspring(ServerLevel pLevel, AgeableMob pOtherParent) {
-        return ModEntities.WEREWOLF.get().create(pLevel);
-    }
+
+
 
     private void setupAnimationStates() {
-        if(this.idleAnimationTimeout <= 0) {
-            this.idleAnimationTimeout = 40;
+        if (this.idleAnimationTimeout <= 0) {
+            this.idleAnimationTimeout = this.random.nextInt(40) + 80;
             this.idleAnimationState.start(this.tickCount);
         } else {
-            --this.idleAnimationTimeout;
+            this.idleAnimationTimeout--;
         }
+
+
+        this.sitAnimationState.startIfStopped(this.tickCount);
+        this.sitPoseAnimationState.stop();
+        this.sitAnimationState.stop();
+        this.idleAnimationState.start(100);
     }
 
     @Override
