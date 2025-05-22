f6b2d700dad7afa05a5242d24bc9f45664e922e6
BT_2S14_11 <michaelk368@nycstudents.net>
2025-05-22 10:37:10 -0400

    first attempt at generating cryonite ore naturally

--- src/main/java/net/dannykandmichaelk/firstmod/worldgen/ModConfiguredFeatures.java
+++ src/main/java/net/dannykandmichaelk/firstmod/worldgen/ModConfiguredFeatures.java
@@ -1,18 +1,36 @@
 package net.dannykandmichaelk.firstmod.worldgen;
 
 import net.dannykandmichaelk.firstmod.FirstMod;
+import net.dannykandmichaelk.firstmod.block.ModBlocks;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
+import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
+import net.minecraft.world.level.levelgen.structure.templatesystem.RuleTest;
+import net.minecraft.world.level.levelgen.structure.templatesystem.TagMatchTest;
+
+import java.util.List;
+
 
 public class ModConfiguredFeatures {
+    public static final ResourceKey<ConfiguredFeature<?, ?>> OVERWORLD_CRYONITE_ORE_KEY = registerKey("cryonite_ore");;
+
 
     public static void bootstrap(BootstrapContext<ConfiguredFeature<?, ?>> context) {
+        RuleTest stoneReplaceables = new TagMatchTest(BlockTags.STONE_ORE_REPLACEABLES);
+        RuleTest deepslateReplaceables = new TagMatchTest(BlockTags.DEEPSLATE_ORE_REPLACEABLES);
 
+        List<OreConfiguration.TargetBlockState> overworldCryoniteOres = List.of(
+                OreConfiguration.target(stoneReplaceables, ModBlocks.CRYONITE_ORE.get().defaultBlockState()),
+                OreConfiguration.target(deepslateReplaceables, ModBlocks.DEEPSLATE_CRYONITE_ORE.get().defaultBlockState()));
+
+        register(context, OVERWORLD_CRYONITE_ORE_KEY, Feature.ORE, new OreConfiguration(overworldCryoniteOres, 9));
 
     }
 
